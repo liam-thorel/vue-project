4e0a4070f6a4153a442b32b1fc563448c49877b9
Liam Thorel <liam.thorel@terconseil.fr>
2023-02-10 15:29:06 +0100

    Recpie show

---
+++ src/components/login-popup.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="login-page">
+    <div class="login-form">
+      <h1>Connexion</h1>
+      <form>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Votre email" required>
+        <label for="password">Mot de passe</label>
+        <input type="password" id="password" name="password" placeholder="Votre mot de passe" required>
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+}
+</script>
+
+<style>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 400px;
+  border: 3px solid goldenrod;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f2f2f2;
+}
+.login-form h1 {
+  font-size: 30px;
+  font-weight: bold;
+  font-family: 'sans-serif', sans-serif;
+}
+.login-form form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.login-form form label {
+  font-size: 20px;
+  font-weight: bold;
+  font-family: 'sans-serif', sans-serif;
+}
+.login-form form input {
+  width: 300px;
+  height: 30px;
+  border: 3px solid goldenrod;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f2f2f2;
+}
+.login-form form button {
+  width: 300px;
+  height: 30px;
+  border: 3px solid goldenrod;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  background-color: #f2f2f2;
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: 'sans-serif', sans-serif;
+}
+</style>
---
+++ src/views/RecipeDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class = "top">
+    <h2> {{ recipe.name }} </h2>
+    <span class = "difficulte" v-for="index in 5" :key="index">
+      <span v-if="index <= recipe.difficulty">★</span>
+      <span v-else>☆</span>
+    </span>
+  </div>
+  <p> {{ recipe.description }} </p>
+  <p> {{ recipe.price }} </p>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: 'RecipeDetails',
+  data () {
+    return {
+      recipe: null,
+    }
+  },
+mounted () {
+  axios.get('https://projet-node-js.vercel.app/recette/').then(response => (this.recipe = response.data, console.log(this.recipe)))
+},
+}
+
+</script>
+<style>
+
+</style>
