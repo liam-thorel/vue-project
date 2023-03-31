3fae939d67e8db88bfaa249c0495e512777e8c12
Liam Thorel <liam.thorel@terconseil.fr>
2023-03-31 23:36:32 +0200

    CreateOrEdit.vue

---
+++ src/components/CreateOrEdit.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="name">Nom</label>
+        <input v-bind:value="name" type="text" class="form-control" id="name" placeholder="Nom de la recette">
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input v-bind:value="description" type="text" class="form-control" id="description" placeholder="Description de la recette">
+      </div>
+      <div class="form-group">
+        <label for="difficulty">Difficulté</label>
+        <input v-bind:value="difficulty" type="number" class="form-control" id="difficulty" placeholder="Difficulté de la recette">
+      </div>
+      <div class="form-group">
+        <label for="price">Prix</label>
+        <input v-bind:value="price" type="number" class="form-control" id="price" placeholder="Prix de la recette">
+      </div>
+      <div class="form-group">
+        <label for="picture">Image</label>
+        <input v-bind:value="picture" type="text" class="form-control" id="picture" placeholder="Image de la recette">
+      </div>
+      <div class="form-group">
+        <label for="nbPerson">Nombre de personnes</label>
+        <input v-bind:value="nbPerson" type="number" class="form-control" id="nbPerson" placeholder="Nombre de personnes pour la recette">
+      </div>
+      <div class="form-group">
+        <label for="weight">Poids</label>
+        <input v-bind:value="weight" type="number" class="form-control" id="weight" placeholder="Poids de la recette">
+      </div>
+      <button @click.prevent="click" class="btn btn-primary">Envoyer</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "CreateOrEdit",
+  props: {
+    name: String,
+    description: String,
+    difficulty: Number,
+    price: Number,
+    picture: String,
+    nbPerson: Number,
+    weight: Number,
+  },
+  methods: {
+    click() {
+    },
+  },
+}
+</script>
