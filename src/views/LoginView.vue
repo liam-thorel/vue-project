29bd7941bde21ce9dc9280b094d19062e10b88e8
Liam Thorel <liam.thorel@terconseil.fr>
2023-03-31 21:48:14 +0200

    Debut route axios

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,18 +3,40 @@
     <div class="login-form">
       <h1>Connexion</h1>
       <form>
-        <label for="nom">Nom</label>
-        <input type="text" id="nom" name="nom" placeholder="Votre nom" required>
+        <label for="name">Nom</label>
+        <input v-model="name" type="text" id="name" name="name" placeholder="Votre nom" required>
         <label for="password">Mot de passe</label>
-        <input type="password" id="password" name="password" placeholder="Votre mot de passe" required>
-        <button type="submit">Se connecter</button>
+        <input v-model="password" type="password" id="password" name="password" placeholder="Votre mot de passe" required>
+        <button type="submit" @click.prevent="login">Se connecter</button>
       </form>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   name: 'LoginPage',
+  data() {
+    return {
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      const user = {
+        name: this.name,
+        password: this.password
+      }
+      console.log(user)
+      axios.post('https://projet-node-js.vercel.app/recettes/connexion', user)
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+  }
+},
 }
 </script>
 
