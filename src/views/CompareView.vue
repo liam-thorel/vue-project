6963b0aef5c0528a15409529fe666f2d8da2e067
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2023-03-31 20:53:40 +0200

    CompareVue End

--- src/views/CompareView.vue
+++ src/views/CompareView.vue
@@ -15,6 +15,19 @@
                    :nbPerson="recipe1.nbPerson"
                    :weight="recipe1.weight">
       </DetailsCard>
+      <div class="compare"  v-show="recipe1.name != null && recipe2.name != null ">
+        <div class="titre">
+          <h3> {{recipe1.name}} </h3>
+          <h3>vs</h3>
+          <h3> {{recipe2.name}} </h3>
+        </div>
+
+        <p>Difficulté : {{difDifficulte}}</p>
+        <p>Personne : {{difPersonne}}</p>
+        <p>Poids : {{difPoids}} kg</p>
+        <p>Prix : {{difPrix}} €</p>
+        <p>Prix/Personne : {{difPrixPersonne}} €</p>
+      </div>
       <DetailsCard v-show="recipe2.picture != null"
                    :name="recipe2.name"
                    :description="recipe2.description"
@@ -49,6 +62,44 @@ export default {
     axios.get('https://projet-node-js.vercel.app/recette/'+this.$route.params.id1).then(response => (this.recipe1 = response.data, console.log(this.recipe1)));
     axios.get('https://projet-node-js.vercel.app/recette/'+this.$route.params.id2).then(response => (this.recipe2 = response.data, console.log(this.recipe2)));
   },
+  computed: {
+    difPersonne() {
+      if (this.recipe1.nbPerson > this.recipe2.nbPerson) {
+        return this.recipe1.nbPerson - this.recipe2.nbPerson;
+      } else {
+        return this.recipe2.nbPerson - this.recipe1.nbPerson;
+      }
+    },
+    difPrix() {
+      if (this.recipe1.price > this.recipe2.price) {
+        return this.recipe1.price - this.recipe2.price;
+      } else {
+        return this.recipe2.price - this.recipe1.price;
+      }
+    },
+    difPoids() {
+      if (this.recipe1.weight > this.recipe2.weight) {
+        return this.recipe1.weight - this.recipe2.weight;
+      } else {
+        return this.recipe2.weight - this.recipe1.weight;
+      }
+    },
+    difDifficulte() {
+      if (this.recipe1.difficulty > this.recipe2.difficulty) {
+        return this.recipe1.difficulty - this.recipe2.difficulty;
+      } else {
+        return this.recipe2.difficulty - this.recipe1.difficulty;
+      }
+    },
+    difPrixPersonne() {
+      if (this.recipe1.price / this.recipe1.nbPerson > this.recipe2.price / this.recipe2.nbPerson) {
+        return this.recipe1.price / this.recipe1.nbPerson - this.recipe2.price / this.recipe2.nbPerson;
+      } else {
+        return this.recipe2.price / this.recipe2.nbPerson - this.recipe1.price / this.recipe1.nbPerson;
+      }
+    }
+  }
+
 }
 </script>
 
@@ -66,5 +117,26 @@ export default {
   align-items: center;
   flex-direction: column;
   flex-wrap: nowrap;
+}
+.compare {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid goldenrod;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px;
+  background-color: yellow;
+  box-shadow: rgba(0, 0, 0, 0.117647) 5px 1px 6px, rgba(0, 0, 0, 0.117647) 5px 1px 4px;
+  width: 500px;
+}
+.titre{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: nowrap;
+
 }
 </style>
